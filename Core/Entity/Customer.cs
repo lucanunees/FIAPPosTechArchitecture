af9982e9144f015631c5ffcd2217d796d7093b64
Lucas Nunes <lucas.nunes@miketec.com.br>
2025-12-11 00:39:31 -0300

    Criação do EF CORE

---
+++ Core/Entity/Customer.cs
@@ -0,0 +1,8 @@
+﻿namespace Core.Entity
+{
+    public class Customer : EntityBase
+    {
+        public required string Name { get; set; }
+        public DateTime? BirthDate { get; set; }
+    }
+}
---
+++ Infrastructure/Repository/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Core.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repository
+{
+    public class ApplicationDbContext : DbContext
+    {
+        // Baixamos o nuget Microsoft.EntityFrameworkCore.SqlServer ( somente no projeto Infrastructure )
+        // criamos a conexão com o banco de dados SQL Server no projeto inicial.
+        // passamos a connection string via construtor para o DbContext (Injeção de dependencia)
+        private readonly string _connectionString;
+
+        public ApplicationDbContext(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        // Configuramos as entitades no banco de dados
+        public DbSet<Customer> Customer { get; set; }
+        public DbSet<Book> Book { get; set; }
+        public DbSet<Order> Order { get; set; }
+    }
+}
